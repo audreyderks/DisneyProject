df2aef146b75c929a07070505bca7586b2a0483a
audreyderks <114101247+audreyderks@users.noreply.github.com>
2024-02-15 13:44:55 +0100

    git setup - add all files

---
+++ src/test/java/be/syntra/DisneyProject/DisneyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.syntra.DisneyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
